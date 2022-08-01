f95ce8ea8e245a8f770b417790a05c073d7f48a9
devyad24 <devyadav4211@gmail.com>
2022-08-01 12:23:06 +0530

    Implement win functionality.

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ const player = (playerName) => {
 // gameBoard.renderGameArray();
 const gameControl = (() => {
     const gbarray = gameBoard.tictacArr;
+    const boxesArr = Array.from(gameBoard.boxes);
+    const currentTurn = document.querySelector(".gameStatus");
     function _boxOccupied(box){
         if(gbarray.length >9 || box.textContent!=="") return true;
     }
-    function playerTurn(){
+    function _playerTurn(){
         const playerA = player("X");
         const playerB = player("O");
         gameBoard.boxes.forEach(box => box.addEventListener('click',e => {
@@ -22,19 +24,35 @@ const gameControl = (() => {
             if(gbarray[gbarray.length-1]===playerB.getPlayerName || gbarray.length===0){
                 e.target.textContent = playerA.getPlayerName;
                 gbarray.push(playerA.getPlayerName);
-                displayTurn(playerB.getPlayerName);
+                _displayTurn(playerB.getPlayerName);
             }else{
                 e.target.textContent = playerB.getPlayerName;
                 gbarray.push(playerB.getPlayerName);
-                displayTurn(playerA.getPlayerName);
+                _displayTurn(playerA.getPlayerName);
             }
         }
+        winCondition(playerA.getPlayerName,playerB.getPlayerName);
         }))
     }
-    function displayTurn(player){
-        const currentTurn = document.querySelector(".gameStatus");
+    function _displayTurn(player){
         currentTurn.textContent = `Player ${player}'s turn.`;
     }
-    playerTurn();
+    function winCondition(playerX,playerO){
+        if((boxesArr[0].textContent===playerX && boxesArr[1].textContent===playerX && boxesArr[2].textContent===playerX) || (boxesArr[0].textContent===playerX && boxesArr[3].textContent===playerX && boxesArr[6].textContent===playerX) || 
+        (boxesArr[6].textContent===playerX && boxesArr[7].textContent===playerX && boxesArr[8].textContent===playerX) || (boxesArr[2].textContent===playerX && boxesArr[5].textContent===playerX && boxesArr[8].textContent===playerX) ||
+        (boxesArr[0].textContent===playerX && boxesArr[4].textContent===playerX && boxesArr[8].textContent===playerX) || (boxesArr[2].textContent===playerX && boxesArr[4].textContent===playerX && boxesArr[6].textContent===playerX) ||
+        (boxesArr[3].textContent===playerX && boxesArr[4].textContent===playerX && boxesArr[5].textContent===playerX) || (boxesArr[1].textContent===playerX && boxesArr[4].textContent===playerX && boxesArr[7].textContent===playerX))
+        {
+            currentTurn.textContent = `${playerX} Won!`;
+        }
+        else if((boxesArr[0].textContent===playerO && boxesArr[1].textContent===playerO && boxesArr[2].textContent===playerO) || (boxesArr[0].textContent===playerO && boxesArr[3].textContent===playerO && boxesArr[6].textContent===playerO) || 
+        (boxesArr[6].textContent===playerO && boxesArr[7].textContent===playerO && boxesArr[8].textContent===playerO) || (boxesArr[2].textContent===playerO && boxesArr[5].textContent===playerO && boxesArr[8].textContent===playerO) ||
+        (boxesArr[0].textContent===playerO && boxesArr[4].textContent===playerO && boxesArr[8].textContent===playerO) || (boxesArr[2].textContent===playerO && boxesArr[4].textContent===playerO && boxesArr[6].textContent===playerO) ||
+        (boxesArr[3].textContent===playerO && boxesArr[4].textContent===playerO && boxesArr[5].textContent===playerO) || (boxesArr[1].textContent===playerO && boxesArr[4].textContent===playerO && boxesArr[7].textContent===playerO))
+        {
+            currentTurn.textContent = `${playerO} Won!`;
+        }
+    }
+    _playerTurn();
     return {gbarray}
 })();
